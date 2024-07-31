5c53d4dd6a1e6f5034d1f24cfe06a33968a6eaec
Ariadna de los Angeles Coronel <ariadnacoronel127@gmail.com>
2024-07-31 20:29:59 -0300

    css de product detail

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector('#menu');
 const ulMenu = document.querySelector('#ul-menu');
 const cerrarMenu = document.querySelector('#cerrar-menu');
-const bodyClass = document.querySelector('body');
 
 menu.addEventListener('click', ()=>{
     ulMenu.classList.add('visible');
@@ -10,5 +9,4 @@ menu.addEventListener('click', ()=>{
 
 cerrarMenu.addEventListener('click', ()=>{
     ulMenu.classList.remove('visible');
-    bodyClass.classList.remove('shadow');
 })
